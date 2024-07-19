c499806d7748752f3fa138af2ab9472a7cb2f6ee
dylantigo <dylannoeltigomo@gmail.com>
2024-07-19 14:02:43 +0100

    Update with possibility to navigate using navBar from projet page to home and the custom hook useNavigueur

--- src/Utilities/projectsImages.js
+++ src/Utilities/projectsImages.js
@@ -1,4 +1,4 @@
-//Ici sera importé les differents images de projets
 import project1 from "../assets/ProjectImage/project1.png"
 
+//Ici sera importé les differents images de projets
 export const imagesTab = [project1]
